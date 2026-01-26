fbb2b41ad30104232498db99f808ffeff030637b
Mansi Thakkar <mansithakkar210@gmail.com>
2026-01-26 09:52:55 -0500

    Initial Commit

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Mansi Thakkar on 2026-01-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
